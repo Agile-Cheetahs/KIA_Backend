016efcf562d73aa7dac6ebd73ffbaf78306a1f48
Danial <danibazi9@gmail.com>
2024-03-20 14:14:27 -0700

    Complete and fix the models for Inventory items and Inventory

--- inventory/models.py
+++ inventory/models.py
@@ -1,28 +1,45 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
 from account.models import Account
-# Create your models here.
 
 
 class InventoryItem(models.Model):
-    name = models.CharField(max_length=100)
+    item_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40)
     quantity = models.IntegerField()
-    units = models.CharField(max_length=50)
-    location = models.CharField(max_length=100)
-    expiration = models.DateField(blank=True, null=True)
-    category = models.CharField(max_length=100, blank=True, null=True)
+    UNITS_CHOICES = [
+        ('lb', 'lb'),
+        ('oz', 'oz'),
+        ('g', 'g'),
+        ('kg', 'kg'),
+        ('count', 'count'),
+        ('t', 't'),
+    ]
+    unit = models.CharField(max_length=10, choices=UNITS_CHOICES)
+    LOCATION_CHOICES = [
+        ('Pantry', 'Pantry'),
+        ('Kitchen', 'Kitchen'),
+        ('Cabinet', 'Cabinet'),
+    ]
+    location = models.CharField(max_length=10, choices=LOCATION_CHOICES)
+    expiration_date = models.DateField(null=True, blank=True)
+    CATEGORY_CHOICES = [
+        ('Fruit', 'Fruit'),
+        ('Grocery', 'Grocery'),
+    ]
+    category = models.CharField(max_length=10, choices=LOCATION_CHOICES, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.quantity} {self.unit}"
+
 
 class Inventory(models.Model):
-    user = models.ForeignKey(Account, on_delete=models.CASCADE)
-    items = models.ManyToManyField(InventoryItem)
+    inventory_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(Account, related_name='inventory', on_delete=models.CASCADE)
+    items = models.ManyToManyField(InventoryItem, related_name='items', blank=True)
+
+    class Meta:
+        verbose_name_plural = "Inventories"
 
     def __str__(self):
-        return f"{self.user.name}'s Inventory"
+        return f"Inventory for {self.user.__str__()}"
