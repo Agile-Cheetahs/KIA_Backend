8f1cc01861b1cd57c9bcec5c348c22b63a6cd67a
Danial <danibazi9@gmail.com>
2024-03-20 15:47:32 -0700

    Fix some issues in routing (URLs)

--- KIA_Backend/urls.py
+++ KIA_Backend/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/account/', include('account.api.urls')),
+    path('api/inventory/', include('inventory.api.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
---
+++ inventory/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from inventory.api import views
+
+app_name = 'inventory'
+
+urlpatterns = [
+    path('all/', views.get_all_inventories, name='get_all_inventories'),
+    path('me/', views.get_my_inventories, name='get_my_inventory'),
+    path('items/', views.InventoryItemCRUD.as_view(), name='inventory_items'),
+]
